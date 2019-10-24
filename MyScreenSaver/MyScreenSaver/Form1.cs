995a10ff747b929ed7070f3efc9da54c18f26c37
KelseySlavin <klslavin412@gmail.com>
2019-10-23 18:18:57 -0700

    Screen Saver Project

---
+++ MyScreenSaver/MyScreenSaver/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyScreenSaver
+{
+    public partial class FormScreenSaver : Form
+    {
+        public FormScreenSaver()
+        {
+            InitializeComponent();
+        }
+
+        private void FormScreenSaver_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FormScreenSaver_KeyDown(object sender, KeyEventArgs e)
+        {
+            Close();
+        }
+    }
+}
